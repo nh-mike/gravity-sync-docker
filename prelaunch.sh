87e2445b7bcae90c9ee20b6e1e847ee07592cf51
ekrekeler <eric.krekeler@gmail.com>
2021-02-19 14:42:36 -0600

    Fix errors

--- prelaunch.sh
+++ prelaunch.sh
@@ -7,9 +7,9 @@ readYesNo()
     answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
     stty $old_stty_cfg
     if echo "$answer" | grep -iq "^y" ;then
-        echo True
+        true
     else
-        echo False
+        false
     fi
 }
 
@@ -19,30 +19,29 @@ echo "What is the hostname / IP of the remote host?"
 read remote_host;
 
 echo "Is the remote SSH server on port 22?"
-if [ readYesNo ]; then
+if readYesNo; then
     remote_port="22"
-else;
+else
     echo "What is the port of your remote SSH server?"
     read remote_port;
 fi
 
-echo "We reccomend creating a gravitysync user on the remote system - would you like to do this?"
-if [ readYesNo ]; then
+echo "We recomend creating a gravitysync user on the remote system - would you like to do this?"
+if readYesNo; then
     #Connect and create user
     remote_user="gravitysync"
     echo "Please provide a management user with sudo privileges"
     echo "We will use this user to create the gravitysync user only"
     echo "ssh -P${remote_port} USER@${remote_host}"
     read username;
-    ssh -o "StrictHostKeyChecking no" ${username}@${remote_host} \
+    ssh -t -o "StrictHostKeyChecking no" ${username}@${remote_host} ' \
         sudo adduser gravitysync && \
         sudo usermod -a -G sudo gravitysync && \
-        sudo usermod -a -G docker gravitysync
-    
-    echo "The remote system is configured as reccomended"
+        sudo usermod -a -G docker gravitysync'
+    echo "The remote system is configured as recomended"
 else
-    echo "Would you like to use the root account? NOT RECCOMENDED!"
-    if [ !readYesNo ]; then
+    echo "Would you like to use the root account? NOT RECOMENDED!"
+    if !readYesNo; then
         echo "What user would you like to connect to on the remote system?"
         echo "Please note that this user must be part of the sudo and docker groups, and must have passwordless sudo"
         read remote_user;
@@ -56,6 +55,6 @@ echo "We will now copy the SSH Key to the remote system. Please provide a passwo
 ssh-copy-id ${remote_user}@${remote_host}
 
 echo "Installing dependencies rsync, sqlite3, git and verifying system"
-ssh -o BatchMode=yes -p${remote_port} ${remote_user}@${remote_host} \
+ssh -o BatchMode=yes -p${remote_port} ${remote_user}@${remote_host} '\
     sudo apt install -y rsync sqlite3 git && \
-    sudo curl -sSL https://gravity.vmstan.com | sudo GS_INSTALL=primary bash
+    sudo curl -sSL https://gravity.vmstan.com | sudo GS_INSTALL=primary bash'
